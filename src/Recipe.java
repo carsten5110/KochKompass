b3ed206463e804cbf615cd47faac5b4e1557fb97
carsten5110 <154989978+carsten5110@users.noreply.github.com>
2024-03-02 12:51:43 +0100

    Initial commit

---
+++ src/Recipe.java
@@ -0,0 +1,11 @@
+public class Rezept {
+    private String recipeTitle;
+    private String author;
+    private int servings;
+    private int calories;
+    private int prepTime;
+    private int cookTime;
+
+
+
+}
